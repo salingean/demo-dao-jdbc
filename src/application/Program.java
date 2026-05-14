0dc035bcf7d19115018c38cbf7ca95f49c0b57b2
Salin Gean <salingeanestrela@gmail.com>
2026-05-14 13:38:03 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,6 +31,11 @@ public class Program {
         for (Seller obj : list) {
             System.out.println(obj);
         }
+
+        System.out.println("\n=== TEST 3: seller insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
     }
 
 }
